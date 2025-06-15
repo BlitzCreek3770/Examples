e4073a0e05a4c61ff64c8fe860942f39f114e545
teklingl <teklingl@gmail.com>
2025-06-15 13:07:37 -0400

    Updated version.

--- 0_JavaFundamentals/BasicConsoleLogging/BasicConsoleLogging.java
+++ 0_JavaFundamentals/BasicConsoleLogging/BasicConsoleLogging.java
@@ -1,17 +1,15 @@
 /*
-  Team 3770 Robotics Programming
-  Example: BasicConsoleLogging
-  Demonstrates variable declaration, data types, and
-  basic Java console output
+  Team 3770 Robotics Programming - Basic Java
+  Demonstrates variable declaration, data types, and basic Java console output.
 */
 public class BasicConsoleLogging
 {
    public static void main( String args[] )
    {
       // Variable declarations
-      int    teamNum   = 3770;
-      double score     = 99.9;
-      String teamName  = "BlitzCreek";
+      int    teamNum   = 3770;            // Declare integer (non-decimal) value
+      double score     = 99.9;            // Declare decimal value
+      String teamName  = "BlitzCreek";    // Declare string value
 
       // Various statements that write to the console
       System.out.println("Welcome to FRC Java Programming");
